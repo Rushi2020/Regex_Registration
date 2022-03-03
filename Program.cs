09d0b832dbf0bd5179f530cfb92b694322cf83ef
Rushi <189489rishi@gmail.com>
2022-03-03 18:57:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//  User registration using regex
+
+using System;
+
+namespace Regex_Registration
+{
+    class program
+    {
+        static void Main (string[] args)
+        {
+            UserRegPattern userRegPattern = new UserRegPattern();
+            string[] checkFirstName = { "Rushi" };
+            userRegPattern.ValidateFirstName(checkFirstName);
+        }
+    }
+}
---
+++ UserRegPattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex_Registration
+{
+    internal class UserRegPattern
+    {
+        public static string FirstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+
+        public void ValidateFirstName(string[] name)
+        {
+            foreach (string n in name)
+            {
+                string result = Regex.IsMatch(n, FirstName) ? $"{n} entered is valid" : $"{n} entered is Invalid";
+
+                Console.WriteLine(result);
+            }
+
+        }
+
+
+    }
+}
